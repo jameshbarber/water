2f0045086082fd17b184a3bfb79139563d2f5579
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-01 21:02:21 +0200

    create initial module sketch

---
+++ src/lib/modules/module.ts
@@ -0,0 +1,68 @@
+import { z } from "zod";
+
+
+type EndpointSchemas = {
+    post?: z.ZodSchema;
+    get?: z.ZodSchema;
+    put?: z.ZodSchema;
+    delete?: z.ZodSchema;
+}
+
+export default class Module {
+    private dbSchema: any;
+    private name: string;
+    private exposeEndpoints: boolean;
+    private endpointSchemas: EndpointSchemas;
+
+    constructor(schema: any, name: string, exposeEndpoints: boolean, endpointSchemas: EndpointSchemas) {
+        this.dbSchema = schema;
+        this.name = name;
+        this.exposeEndpoints = exposeEndpoints;
+        this.endpointSchemas = endpointSchemas;
+    }
+
+    createEndpoints() {
+        const endpoints = [];
+
+        return endpoints;
+    }
+
+    createServices() {
+        const services = [];
+    }
+
+    createControllers() {
+        const controllers = [];
+
+        return controllers;
+    }
+    
+    createModels() {
+        const models = [];
+
+        return models;
+    }
+    
+    createEvents() {
+        const events = [];
+
+        return events;
+    }
+    
+    
+    createSubscribers() {
+        const subscribers = [];
+
+        return subscribers;
+    }
+    
+    
+    
+    createListeners() {
+        const listeners = [];
+
+        return listeners;
+    }
+    
+    
+}
