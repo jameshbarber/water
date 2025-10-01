98d7d7ba4fb71b6ee9d0c88974fed5c83d26d444
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-01 21:01:45 +0200

    add testing tooling & first test sketch

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!**/node_modules/**', '!**/dist/**'],
+  setupFiles: ['dotenv/config'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default config;
