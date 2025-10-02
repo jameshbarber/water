70de0c4cca431304b5a508ecf8ee8f3d6f10d906
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-02 21:46:13 +0200

    fix db adapter typing

--- src/core/dependencies/db/index.ts
+++ src/core/dependencies/db/index.ts
@@ -1,8 +1,8 @@
-export type Where<T> = Partial<Record<keyof T, any>>;
+export type Where<T> = Partial<T>;
 
-export interface DatabaseAdapter<T> {
-    findOne(args: { where: Where<Partial<T>> }): Promise<T | null>;
-    findMany(args: { where?: Where<Partial<T>> }): Promise<T[]>;
+export interface DatabaseAdapter<T extends { id: string }> {
+    findOne(args: { where: Where<T> }): Promise<T | null>;
+    findMany(args: { where?: Where<T> }): Promise<T[]>;
     create(args: { data: T | Omit<T, "id"> }): Promise<T>;
     update(args: { where: Where<T>; data: Partial<T> }): Promise<T | null>;
     delete(args: { where: Where<T> }): Promise<T | null>;
