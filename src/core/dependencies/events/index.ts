b9ecae00a3c37487aceb245cc80c54a9d586c26f
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-02 21:45:03 +0200

    use abstract class for event bus

--- src/core/dependencies/events/index.ts
+++ src/core/dependencies/events/index.ts
@@ -1,4 +1,12 @@
-interface EventBus {
-    emit(eventName: string, data: any): void;
-    on(eventName: string, callback: (data: any) => void): void;
+import { Logger } from "../logger";
+
+export abstract class EventBus {
+    private logger: Logger;
+
+    constructor(logger: Logger) {
+        this.logger = logger;
+    }
+
+    abstract emit(eventName: string, data: any): void;
+    abstract on(eventName: string, callback: (data: any) => void): void;
 }
