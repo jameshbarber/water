c7f9c4e993c07547bee92c95bf153787c4e10336
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-02 21:43:50 +0200

    add support for wildcard event listeners

--- src/adapters/events.ts
+++ src/adapters/events.ts
@@ -1,14 +1,29 @@
-class SimpleEventBus implements EventBus {
+import { EventBus } from "@/core/dependencies/events";
+import { Logger } from "@/core/dependencies/logger";
+
+class SimpleEventBus extends EventBus {
     private events: { [key: string]: ((data: any) => void)[] } = {};
+    private wildcardEvents: ((data: any) => void)[] = [];
+
+    constructor(logger: Logger) {
+        super(logger);
+    }
 
     emit<T>(eventName: string, data: T) {
         if (!this.events[eventName]) {
             this.events[eventName] = [];
         }
+        this.wildcardEvents.forEach(callback => callback(data));
         this.events[eventName].forEach(callback => callback(data));
     }
 
     on<T>(eventName: string, callback: (data: T) => void) {
+        if (!eventName) {
+            throw new Error("Event name is required");
+        }
+        if (eventName === "*") {
+            this.wildcardEvents.push(callback as (data: any) => void);
+        }
         if (!this.events[eventName]) {
             this.events[eventName] = [];
         }
