2472266c0ca6fc6b817a1d648fa0a229bac89b17
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-03 11:05:03 +0200

    export triggers factory from module root

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { AppManifest } from "./core/app";
 import App from "./core/app";
 import { deviceSchemaProvider } from "./modules/devices";
 import { devicesModuleFactory } from "./modules/devices/factory";
-import { triggerSchemaProvider } from "./modules/triggers/schema";
-import { triggersModuleFactory } from "./modules/triggers/factory";
+import { triggerSchemaProvider, triggersModuleFactory } from "./modules/triggers";
 import { Driver } from "./core/dependencies/drivers";
 import { createSubscribers } from './subscribers'
 
--- src/modules/triggers/index.ts
+++ src/modules/triggers/index.ts
@@ -1,7 +1,8 @@
 import TriggerModule from "./module";
 import { triggerSchemaProvider, triggerSchema } from "./schema";
-
+import { triggersModuleFactory } from "./factory";
 
 export default TriggerModule;
 export { TriggerModule };
 export { triggerSchemaProvider, triggerSchema };
+export { triggersModuleFactory };
