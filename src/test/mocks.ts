444096a0c3f02a028e9de6d1cb0ca923c7f6e05b
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-03 17:43:15 +0200

    update test mocks

--- src/test/mocks.ts
+++ src/test/mocks.ts
@@ -2,9 +2,27 @@ import { ZodSchemaProvider } from "@/adapters/schema";
 import type { SchemaProvider } from "@/adapters/schema/types";
 import type { EventBus } from "@/core/dependencies/events";
 import { ModuleConfig } from "@/core/modules";
+import { Deps } from "@/deps";
 import { z } from "zod";
 
 export const makeDbSchema = () => {
+  return {
+    findOne: jest.fn(),
+    findMany: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  } as any;
+};
+
+export const makeDb = () => {
+  const singleton = makeDbRepo();
+  return {
+    repo: jest.fn((_key: string) => singleton),
+  } as any;
+};
+
+export const makeDbRepo = () => {
   return {
     findOne: jest.fn(),
     findMany: jest.fn(),
@@ -21,41 +39,57 @@ export const makeEventBus = () => {
   } as unknown as EventBus;
 };
 
-export const makeSchema = (schema: z.ZodObject): SchemaProvider => {
+export const makeSchema = (schema: z.ZodObject): SchemaProvider<any> => {
   const schemaProvider = new ZodSchemaProvider(schema);
-  return schemaProvider as unknown as SchemaProvider;
+  return schemaProvider as unknown as SchemaProvider<any>;
 };
 
 export const makeModuleConfig = (schema: z.ZodObject) => {
   return {
-    store: makeDbSchema(),
     name: "settings",
-    eventBus: makeEventBus(),
-    schema: makeSchema(schema),
-    logger: makeLogger(),
+    schemas: makeSchema(schema),
   } as ModuleConfig<any>;
 };
 
+let __driverSingleton: any;
 export const makeDriver = () => {
-  return {
-    write: jest.fn(),
-    read: jest.fn(),
-  } as any;
+  if (!__driverSingleton) {
+    __driverSingleton = {
+      write: jest.fn(),
+      read: jest.fn(),
+    } as any;
+  }
+  return __driverSingleton;
 };
 
 
 
-export const makeDeps = () => {
+export const makeDeps = (): Deps => {
   return {
-    db: makeDbSchema(),
+    makeModuleDataStore: ()=>(schemas: SchemaProvider<any>)=>jest.fn(),
+    logger: makeLogger(),
     eventBus: makeEventBus(),
+    database: makeDb(),
+    driver: makeDriver(),
   } as any;
 };
 
 export const makeLogger = () => {
   return {
+    debug: jest.fn(),
     info: jest.fn(),
     warn: jest.fn(),
     error: jest.fn(),
+  } as any;
+};
+
+
+export const makeStore = () => {
+  return {
+    findOne: jest.fn(),
+    findMany: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   } as any;
 };
