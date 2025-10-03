e4c0a4480d462589ee1f9ec0983b45b0db3cd571
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-03 17:35:36 +0200

    rename manifest to config

---
+++ src/config.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import { AppManifest } from "./core/app";
+import CommandsModule, { commandSchemaProvider } from "./modules/commands";
+import DevicesModule, { deviceSchemaProvider } from "./modules/devices";
+import { triggerSchemaProvider } from "./modules/triggers";
+
+const manifest: AppManifest = {
+    "name": "water",
+    "version": "1.0.0",
+    "interfaces": {
+        "mcp": {
+            "enabled": false,
+        },
+        "rest": {
+            "enabled": true,
+            "port": 4000,
+            "host": "0.0.0.0"
+        }
+    },
+    "store": {
+        "type": "json", 
+        "url": "data.json"
+    },
+    "modules": {
+        "commands": {
+            "name": "commands",
+            "schemas": commandSchemaProvider,
+            "constructor": CommandsModule
+        },
+        "devices": {
+            "name": "devices",
+            "schemas": deviceSchemaProvider,
+            "constructor": DevicesModule
+        },
+        "triggers": {
+            "name": "triggers",
+            "schemas": triggerSchemaProvider
+        }
+    }
+}
+
+export default manifest;
