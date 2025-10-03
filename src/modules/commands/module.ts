3f15b78e10aba863bb5db8e1bae7470009c17e6a
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-03 17:44:50 +0200

    new factory setup

--- src/modules/commands/module.ts
+++ src/modules/commands/module.ts
@@ -3,12 +3,13 @@ import { ModuleConfig } from "@/core/modules";
 import { exec } from "node:child_process";
 import { promisify } from "node:util";
 import { CommandRecord } from "./schema";
+import { Deps } from "@/deps";
 
 const execAsync = promisify(exec);
 
 class CommandsModule extends Module<CommandRecord> {
-    constructor(config: ModuleConfig<CommandRecord>) {
-        super(config);
+    constructor(config: ModuleConfig<CommandRecord>, deps: Deps) {
+        super(config, deps);
     }
 
     async runCommand(commandId: string) {
