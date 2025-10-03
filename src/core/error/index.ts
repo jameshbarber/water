d1b56d1b744f384241c9d2f648017bc89e281441
James Hilton-Barber <jameshiltonbarber@gmail.com>
2025-10-03 10:20:42 +0200

    add detail payload to error

--- src/core/error/index.ts
+++ src/core/error/index.ts
@@ -1,11 +1,13 @@
 class AppError extends Error {
     status?: number;
     code?: string;
+    details?: unknown;
 
-    constructor(message: string, status?: number, code?: string) {
+    constructor(message: string, status?: number, code?: string, details?: unknown) {
         super(message);
         this.status = status;
         this.code = code;
+        this.details = details;
     }
 }
 
